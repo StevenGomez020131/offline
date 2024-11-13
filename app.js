58cb1b5339c6760ff3a2f9c402825ed36ea62298
StevenGomez020131 <colombianodavid2002@gmail.com>
2024-11-13 09:48:01 -0600

    primer

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./service-worker.js')
+    .then(function(registration){
+
+    })
+    .catch(function(error){
+        console.log('Error al registrar el Service Worker:', error)
+    });
+}
